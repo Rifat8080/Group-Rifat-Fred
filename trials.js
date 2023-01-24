52b68da3d45c257061934fb98b39ade3314b5409
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-01-24 20:26:57 +0600

    Update trials.js

--- trials.js
+++ trials.js
@@ -1,1 +1,25 @@
+"use strict"; //Strict mode turn on
+// Md Mahadi Hasan Rifat
+// Breaking Record Hackerrank coding challange
 
+const breakingRecords = function (n) {
+  let maxScore = n[0];
+  let minScore = n[0];
+  let maxBroke = 0;
+  let minBroke = 0;
+  // for loop iteration for traverse all the arrays
+  for (let i = 0; i < n.length; i++) {
+    if (n[i] > maxScore) {
+      maxScore = n[i];
+      maxBroke++;
+    }
+
+    if (n[i] < minScore) {
+      minScore = n[i];
+      minBroke++;
+    }
+  }
+  return [maxBroke, minBroke];
+};
+
+console.log(breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 24, 42]));
