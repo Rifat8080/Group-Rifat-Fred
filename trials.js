09ce883be351d53e75a7420c2b65b8e873f2b50e
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-01-24 21:41:37 +0600

    js trials

--- trials.js
+++ trials.js
@@ -23,3 +23,27 @@ const breakingRecords = function (n) {
 };
 
 console.log(breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 24, 42]));
+
+// Fred offei
+// Minimum Distances Challenge
+
+function minimumDistances(a) {
+  let distances = [];
+  // for loop iteration for traverse all the arrays
+  for (let i = 0; i < a.length; i++) {
+    if (a.indexOf(a[i]) !== a.lastIndexOf(a[i])) {
+      let min = a.lastIndexOf(a[i]) - a.indexOf(a[i]);
+      distances.push(min);
+    }
+  }
+
+  if (distances.length === 0) {
+    return -1;
+  } else {
+    distances.sort(function (a, b) {
+      return a - b;
+    });
+
+    return distances[0];
+  }
+}
